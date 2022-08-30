c5b5007bbb595a5390708b6bd63c093bcf4ea849
Ryuku-sketch <cyamy122002@gmail.com>
2022-08-30 18:25:18 +0900

    Added Camera Function

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,30 +2,38 @@ import 'package:app/view/newcamera.dart';
 import 'package:app/view/title_page.dart';
 import 'package:app/view/camera_view.dart';
 import 'package:app/view/suggested_camera_view.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  // This will list all the available cameras
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+  runApp(MyApp(camera: cameras));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.camera}) : super(key: key);
+  final List<CameraDescription> camera;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sample App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       // This widget is the root of your application.
       initialRoute:  '/',
       routes: {
-        '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+        '/': (context) => const MyHomePage(title: 'Sample App'),
         '/title': (context) => TitleView(),
         '/camera': (context) => CameraView(),
         '/suggested': (context) => SuggestedView(),
-        '/newcamera': (context) => NewCameraView(),
+        '/newcamera': (context) => NewCameraView(camera: camera),
 
       },
     );
--- app/lib/view/newcamera.dart
+++ app/lib/view/newcamera.dart
@@ -1,38 +1,72 @@
 
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class NewCameraView extends StatefulWidget{
-  NewCameraView({Key? key}) : super(key:key);
-
+  NewCameraView({Key? key, required this.camera}) : super(key:key);
+  final List<CameraDescription> camera;
   @override
   State<NewCameraView> createState() => _NewCameraState();
 }
 
 class _NewCameraState extends State<NewCameraView>{
-  int _counter = 0;
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  late XFile image;
 
-  void _increment(){
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState(){
+    super.initState();
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera[0],
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+    _initializeControllerFuture = _controller.initialize();
   }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(15),),
-            Container(
-              child: Text('$_counter')
-            ),
-            TextButton(onPressed: (){_increment();}, child: const Text('Push me'))
-          ],
-        ),
+      body:Column(
+        children: [
+          Container(
+              child: FutureBuilder<void>(
+                  future: _initializeControllerFuture,
+                  builder:  (BuildContext context, snapshot){
+                    if (snapshot.connectionState == ConnectionState.done){
+                      return CameraPreview(_controller);
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  }
+              )
+          ),
+          FloatingActionButton(onPressed: ()async{
+            try{
+              await _initializeControllerFuture;
+              image = await _controller.takePicture();
+              print('Successfully Captured');
+              _controller.initialize();
+            }catch(error){
+              print(error);
+            }
+          }, 
+            child: const Icon(Icons.camera_alt,),
+          ),
+
+        ],
       )
+
     );
   }
-
 }
