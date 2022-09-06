947bd2c16d39bbb022ac42187a4c4a7c9e70e378
Ryuku-sketch <cyamy122002@gmail.com>
2022-09-06 17:38:39 +0900

    refactor

--- app/lib/util/camera_notifier.dart
+++ app/lib/util/camera_notifier.dart
@@ -5,14 +5,17 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 class CameraNotifier extends ChangeNotifier {
+  // Sample Parameter
   String fruit = 'unknown';
 
   List<XFile> picturesList = [];
 
+  // Sample Function
   void changeFruit(String newFruit){
     fruit = newFruit;
     notifyListeners();
   }
+
   void updatePictures(XFile newPic){
     picturesList.add(newPic);
     notifyListeners();
